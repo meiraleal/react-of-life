ffb449b67d1ed81ef54c5d577ab6e82bf2c024bb
Alan Leal <alanmeira@gmail.com>
2018-05-23 18:24:04 -0300

    update tests

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -4,41 +4,42 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { shallow, mount } from 'enzyme';
 import renderer from 'react-test-renderer';
+import GameOfLife from '../containers/GameOfLife';
 import App from './App';
 import Grid from './Grid';
 import Header from './Header';
 import Cell from './Cell';
-import * as Life from './Life';
+import * as Life from '../Life';
 
 configure({ adapter: new Adapter() });
 
 const rows = 50;
 const cols = 50;
-const seed = [[0,0], [0, 1], [1, 0], [1, 3], [2, 1], [2, 2]]; //initialState
-
+const grid = [...Array(rows)].map(e => Array(50).fill(0));
+const generation = 0;
 
 describe('<App />', () => {
 
   it('Be able to take in the following cells [x,y] to set as live, in order to initialize the game: [[0,0], [0, 1], [1, 0], [1, 3], [2, 1], [2, 2]]', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<App {...{rows, cols, seed}}/>, div);
+    ReactDOM.render(<App {...{rows, cols, generation, grid}} />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 
   it('Print out the cells of the grid, either simply as outputted numbers, or (bonus points) rendered to an HTML document.', () => {
     const tree = renderer
-          .create(<App {...{rows, cols, seed}}/>)
+          .create(<App {...{rows, cols, generation, grid}} />)
           .toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   it('should render Header component', () => {
-    const wrapper = shallow(<App {...{rows, cols, seed}}/>);
+    const wrapper = shallow(<App {...{rows, cols, generation, grid}} />);
     expect(wrapper.find(Header)).toHaveLength(1);
   });
 
   it('should render Grid component', () => {
-    const wrapper = shallow(<App {...{rows, cols, seed}}/>);
+    const wrapper = shallow(<App {...{rows, cols, generation, grid}} />);
     expect(wrapper.find(Grid)).toHaveLength(1);
   });
 });
