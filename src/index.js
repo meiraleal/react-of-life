61503d82955128b2ee82adc14550bd8f1cb7c5cf
Alan Leal <alanmeira@gmail.com>
2018-05-24 01:37:50 -0300

    add redux dev tools

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { compose, createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
 import GameOfLife from './containers/GameOfLife';
 
-const store = createStore(
-  reducers,
-  applyMiddleware(thunk)
-);
+const middleware = [thunk];
+// eslint-disable-next-line no-underscore-dangle
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducers, /* preloadedState, */ composeEnhancers(applyMiddleware(...middleware)));
 
 render(
   <Provider store={store}>
